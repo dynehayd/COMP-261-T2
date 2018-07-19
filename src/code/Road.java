41b5d2018ae1b26d15558880d87d96de0b7a510e
hayden dyne <h.dyne666@gmail.com>
2018-07-19 14:09:58 +1200

    first commit

---
+++ src/code/Road.java
@@ -0,0 +1,12 @@
+package code;
+
+public class Road {
+    private Node start;
+    private Node end;
+
+    public Road(Node start, Node end){
+        this.start = start;
+        this.end = end;
+    }
+
+}
---
+++ src/code/Segment.java
@@ -0,0 +1,12 @@
+package code;
+
+import java.util.ArrayList;
+
+public class Segment {
+    ArrayList<Road> roads;
+    ArrayList<Node> nodes;
+
+    public Segment(){
+        
+    }
+}
